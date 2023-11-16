fdf6593bed3079ef028f175f2977869735d41f4c
PraharshithaBattina <119999952+PraharshithaBattina@users.noreply.github.com>
2023-11-16 12:10:35 +0530

    import java.util.*; public class Charm{     public static void main(String[] args){         Scanner n = new Scanner(System.in);         double a;double b;         a=n.nextDouble();         b=32+(a*9/5);         System.out.printf("%.2f",b);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Charm{
+    public static void main(String[] args){
+        Scanner n = new Scanner(System.in);
+        double a;double b;
+        a=n.nextDouble();
+        b=32+(a*9/5);
+        System.out.printf("%.2f",b);
+    }
+}
